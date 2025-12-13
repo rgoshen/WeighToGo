b3046057ca7d19fcd78ff391cc96207358e1a881
Richard Goshen <rick.goshen@gmail.com>
2025-12-12 17:12:15 -0700

    refactor: use convertBetweenUnits in GoalDialogFragment (2 callsites)

--- app/src/main/java/com/example/weighttogo/fragments/GoalDialogFragment.java
+++ app/src/main/java/com/example/weighttogo/fragments/GoalDialogFragment.java
@@ -405,15 +405,7 @@ public class GoalDialogFragment extends DialogFragment {
             String oldUnit = existingGoal.getGoalUnit();
             if (!oldUnit.equals(selectedUnit)) {
                 double currentStartWeight = existingGoal.getStartWeight();
-                // Explicit conversion based on old and new units
-                if ("lbs".equals(oldUnit) && "kg".equals(selectedUnit)) {
-                    // Converting from lbs to kg
-                    goal.setStartWeight(WeightUtils.convertLbsToKg(currentStartWeight));
-                } else if ("kg".equals(oldUnit) && "lbs".equals(selectedUnit)) {
-                    // Converting from kg to lbs
-                    goal.setStartWeight(WeightUtils.convertKgToLbs(currentStartWeight));
-                }
-                // else: no conversion needed (shouldn't happen, but safe)
+                goal.setStartWeight(WeightUtils.convertBetweenUnits(currentStartWeight, oldUnit, selectedUnit));
             }
 
             goal.setGoalUnit(selectedUnit);
@@ -448,16 +440,7 @@ public class GoalDialogFragment extends DialogFragment {
             goal.setGoalUnit(selectedUnit);
 
             // Convert start weight if unit was changed
-            double startWeight = currentWeight;
-            if (!currentUnit.equals(selectedUnit)) {
-                // Explicit conversion based on current and selected units
-                if ("lbs".equals(currentUnit) && "kg".equals(selectedUnit)) {
-                    startWeight = WeightUtils.convertLbsToKg(currentWeight);
-                } else if ("kg".equals(currentUnit) && "lbs".equals(selectedUnit)) {
-                    startWeight = WeightUtils.convertKgToLbs(currentWeight);
-                }
-                // else: no conversion needed (shouldn't happen, but safe)
-            }
+            double startWeight = WeightUtils.convertBetweenUnits(currentWeight, currentUnit, selectedUnit);
             goal.setStartWeight(startWeight);
 
             goal.setTargetDate(selectedTargetDate); // Optional, can be null
